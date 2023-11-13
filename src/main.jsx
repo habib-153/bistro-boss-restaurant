c10f848e4e6f20103acd614557406828c7d08b2e
habib-153 <h.r.sihab155@gmail.com>
2023-11-13 23:43:30 +0600

    Provider set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './Routes/Routes';
-import { HelmetProvider } from 'react-helmet-async';
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-    <div className='max-w-screen-xl mx-auto'>
-    <RouterProvider router={router} />
-    </div>
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
