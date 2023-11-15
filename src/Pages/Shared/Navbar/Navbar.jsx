15b9e9803302fca5a280724a6aa429575aa12d58
habib-153 <h.r.sihab155@gmail.com>
2023-11-16 01:43:55 +0600

    useCart hook, Carts element shown on navbar by specipic user

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -2,8 +2,10 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Provider/AuthProvider";
 import { FaShoppingCart } from 'react-icons/fa';
+import useCart from "../../../Hooks/useCart";
 const Navbar = () => {
   const {user, logOut} = useContext(AuthContext)
+  const [cart] = useCart()
 
   const handleLogOut = () =>{
     logOut()
@@ -25,7 +27,7 @@ const Navbar = () => {
         <Link className="text-white mx-5" to='/'>
         <button className="btn">
             <FaShoppingCart></FaShoppingCart>
-            <div className="badge badge-secondary">+0</div>
+            <div className="badge badge-secondary">+{cart.length}</div>
         </button>
         </Link>
       </li>
